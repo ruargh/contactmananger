5ff3a82b474b93bb6f837574f93baf08986aa33b
Ruargh Steyn <ruargh@gmail.com>
2018-10-03 17:50:51 +0200

    Section 4: State & The Context API - 1. Creating State - START

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@email.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@email.com"
-          phone="333-333-3333"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@email.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@email.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
