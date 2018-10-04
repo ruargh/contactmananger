ee98c93772e63f280ab4b649b0091e1e5704d4b4
Ruargh Steyn <ruargh@gmail.com>
2018-10-04 15:49:11 +0200

    5. Forms | 3. Uncontrolled Components & Refs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,23 @@ class AddContact extends Component {
     phone: ''
   };
 
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -22,6 +32,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Name..."
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -32,6 +43,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Email..."
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -42,6 +54,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Phone..."
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
